dd5a8db8ecc81cce71336ec1ec9c2a2b2cfebc4f
Michael Beyers <mpbeyers@me.com>
2022-08-04 15:57:33 -0700

    Updated File

--- Hello_File.py
+++ Hello_File.py
@@ -1,3 +1,16 @@
 My new file!
 for i in 'hello':
 print ('hello')
+#This is an alteration to the earlier file
+np.random.seed(42)
+x = np.random.randn(100)
+
+bins = np.linspace(-5, 5, 20)
+counts = np.zeros_like(bins)
+
+i = np.searchsorted(bins, x)
+
+np.add.at(counts, i, 1)
+
+#plot results
+plt.plot(bins, counts, linestyle='steps');
